b71497a644234764d7d34888b8a988d1783bd34b
Matheus <matheus.paixao@aevotech.com.br>
2023-02-09 00:16:38 -0300

    Update: add home and perfil routes and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './views/home/home.component';
+import {PerfilComponent} from './views/perfil/perfil.component';
 
-const routes: Routes = [];
+
+export const DEFAULT_ROUTE = '/home';
+
+const routes: Routes = [
+  {path: '', redirectTo: DEFAULT_ROUTE, pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'perfil', component: PerfilComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
